4803185f2cecf7ea0f8c96e5c7e2e4c280d84843
DeAndre Hoff <deandreryanhoff@yahoo.com>
2021-11-10 16:56:06 -0800

    commit tree 5b229ec1f726f4c547112002ff167a51e3accae4

--- src/index.js
+++ src/index.js
@@ -7,9 +7,52 @@ document.addEventListener("DOMContentLoaded", () => {
     // hide & seek with the form
     addToy = !addToy;
     if (addToy) {
-      toyFormContainer.style.display = "block";
+      toyFormContainer.style .display = "block";
     } else {
       toyFormContainer.style.display = "none";
     }
   });
+    fetch("http://localhost:3000/toys")
+    .then(resp => resp.json())
+    .then( function (toys) {
+      renderToys(toys)
+    })
+function renderToys(toys) {
+const toyCollection = document.getElementById('toy-collection')
+
+toys.forEach(element => {
+  console.log(element)
+
+
+  let card = document.createElement('div')
+  card.className = 'card'
+ 
+  let toyName = document.createElement('h2') 
+  toyName.innerText = element.name
+  card.append(toyName)
+
+
+  let toyAvatar = document.createElement('img')
+  toyAvatar.src = element.image
+  toyAvatar.setAttribute('class', 'toy-avatar')
+  card.append(toyAvatar)
+  toyCollection.append(card)
+
+  let toyLikes = document.createElement('p')
+  toyLikes = element.likes
+  card.append(toyLikes)
+
+  let likeButtn = document.createElement('button')
+  likeButtn.id = element.id 
+  likeButtn.innerText = 'like'
+  likeButtn.className = 'like-btn'
+  card.append(likeButtn)
+})
+
+
+   
+}
+
 });
+
+
